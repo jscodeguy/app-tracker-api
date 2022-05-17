448f7cf67d088d99c03d4458c71da97602ed070f
jscodeguy <im.still.at.school99@gmail.com>
2022-05-16 20:48:12 -0400

    patch route for applications

--- app/routes/application_routes.js
+++ app/routes/application_routes.js
@@ -72,4 +72,27 @@ router.post('/application', requireToken, (req, res, next) => {
 		.catch(next)
 })
 
+// UPDATE
+// PATCH /application/5a7db6c74d55bc51bdf39793
+router.patch('/application/:id', requireToken, removeBlanks, (req, res, next) => {
+	// if the client attempts to change the `owner` property by including a new
+	// owner, prevent that by deleting that key/value pair
+	delete req.body.application.owner
+
+	Application.findById(req.params.id)
+		.then(handle404)
+		.then((application) => {
+			// pass the `req` object and the Mongoose record to `requireOwnership`
+			// it will throw an error if the current user isn't the owner
+			requireOwnership(req, application)
+
+			// pass the result of Mongoose's `.update` to the next `.then`
+			return application.updateOne(req.body.application)
+		})
+		// if that succeeded, return 204 and no JSON
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
+
 module.exports = router
