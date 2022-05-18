0024bcc006938fa1c36e5f62814beef9e5ef6d63
jscodeguy <im.still.at.school99@gmail.com>
2022-05-17 21:34:24 -0400

    update route for resume

--- app/routes/resume_routes.js
+++ app/routes/resume_routes.js
@@ -72,4 +72,27 @@ router.post('/resume', requireToken, (req, res, next) => {
 		.catch(next)
 })
 
+// UPDATE
+// PATCH /resume/5a7db6c74d55bc51bdf39793
+router.patch('/resume/:id', requireToken, removeBlanks, (req, res, next) => {
+	// if the client attempts to change the `owner` property by including a new
+	// owner, prevent that by deleting that key/value pair
+	delete req.body.resume.owner
+
+	Resume.findById(req.params.id)
+		.then(handle404)
+		.then((resume) => {
+			// pass the `req` object and the Mongoose record to `requireOwnership`
+			// it will throw an error if the current user isn't the owner
+			requireOwnership(req, resume)
+
+			// pass the result of Mongoose's `.update` to the next `.then`
+			return resume.updateOne(req.body.resume)
+		})
+		// if that succeeded, return 204 and no JSON
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
+
 module.exports = router
